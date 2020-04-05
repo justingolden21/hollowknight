240ba9dc09ddbb590a02ae333ac964b4b459df42
Justin Golden <justingolden21@gmail.com>
2020-04-05 00:43:20 -0700

    added popper

--- js/make-charms.js
+++ js/make-charms.js
@@ -3,7 +3,8 @@ $( ()=> {
 	for(charm in CHARMS) {
 		idx++;
 		$('#charm-div').append('<img class="charm" src="img/charms/' + charm + '.png"' +
-			' title="' + format(charm) + '" tabindex="0" data-charm="' + charm + '"' +
+			' data-toggle="popover" title="' + format(charm) + 
+			'" tabindex="0" data-charm="' + charm + '"' +
 			'" onclick="openCharm(\'' + charm + '\')">');
 		if(idx%10==9) {
 			$('#charm-div').append('<br class="charm-br">');
@@ -12,6 +13,8 @@ $( ()=> {
 			}
 		}
 	}
+	$('[data-toggle="popover"]').popover({trigger: 'hover'});
+
 	$('.charm').keydown( (evt)=> {
 		if(evt.which == 13) // enter
 			openCharm($(':focus').attr('data-charm') );
